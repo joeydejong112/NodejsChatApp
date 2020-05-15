08c6a381ba19fe856857b3840b5952175caee8c5
yes <529521@student.glu.nl>
2020-05-15 03:18:31 +0200

    linkedin course

---
+++ index.js
@@ -0,0 +1,50 @@
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const port = 3000;
+
+
+server.listen(port, () => {
+    console.log(`server is running on port ${port}`)
+});
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+});
+
+app.get('/javascript', (req, res) => {
+    res.sendFile(__dirname + '/public/javascript.html')
+});
+
+app.get('/swift', (req, res) => {
+    res.sendFile(__dirname + '/public/swift.html')
+});
+app.get('/css', (req, res) => {
+    res.sendFile(__dirname + '/public/css.html')
+});
+
+// namespaces
+
+const tech = io.of('/tech');
+
+
+
+tech.on('connection', (socket) => {
+   console.log('user conntected');
+
+   socket.on('join', (data) =>{
+        socket.join(data.room)
+        tech.in(data.room).emit('message' , `New user joined ${data.room} room!`)
+   })
+
+   socket.on('message' , (data) =>{
+       console.log(`message : ${data.msg}`);
+       tech.in(data.room).emit('message' , data.msg);
+   });
+
+   socket.on('disconnect', () =>{
+       console.log('user disconnected');
+    tech.emit('message' , 'user disconnected')
+   })
+})
