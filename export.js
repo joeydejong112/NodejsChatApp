64e68071c73716b8954258c1781ec64cc59797d5
yes <529521@student.glu.nl>
2020-06-07 02:59:46 +0200

    update global chat room

---
+++ export.js
@@ -0,0 +1,26 @@
+
+// create a table
+// pool.query("CREATE TABLE rooms (id INT, name VARCHAR(255))", (err, res) => {
+//   console.log(err, res)
+//   pool.end()
+// })
+
+
+
+// pool.query("CREATE TABLE chats (id SERIAL, username VARCHAR(255) , message VARCHAR(255),  room VARCHAR(255) , tijd TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP)", (err, res) => {
+//     console.log(err, res)
+//     pool.end()
+//   })
+
+// drop a table
+// pool.query("DROP TABLE chats", (err, res) => {
+//   console.log(err, res)
+//   pool.end()
+// })
+
+
+//instert
+// pool.query("", (err, res) => {
+//   console.log(err, res)
+//   pool.end()
+// })
--- index.js
+++ index.js
@@ -5,44 +5,27 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const port = 3000;
 
+
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname);
 //Post gres database
 app.use(express.static(__dirname + '/public'));
 
-const { Pool, Client } = require('pg')
+const {
+    Pool,
+    Client
+} = require('pg')
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'chat',
-  password: 'joeydejong12',
-  port: 5432,
+    user: 'postgres',
+    host: 'localhost',
+    database: 'chat',
+    password: 'joeydejong12',
+    port: 5432,
 })
 
 
-// create a table
-// pool.query("CREATE TABLE users (id INT, username VARCHAR(255) , password VARCHAR(255))", (err, res) => {
-//   console.log(err, res)
-//   pool.end()
-// })
-
-
-
-// pool.query("CREATE TABLE chats (id SERIAL, username VARCHAR(255) , message VARCHAR(255),  room VARCHAR(255) , tijd TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP)", (err, res) => {
-//     console.log(err, res)
-//     pool.end()
-//   })
-
-// drop a table
-// pool.query("DROP TABLE chats", (err, res) => {
-//   console.log(err, res)
-//   pool.end()
-// })
-
-
-//instert
-// pool.query("", (err, res) => {
-//   console.log(err, res)
-//   pool.end()
-// })
 
 
 
@@ -50,105 +33,121 @@ const pool = new Pool({
 
 // Routes
 app.get('/', (req, res) => {
+
     res.sendFile(__dirname + '/public/index.html')
 });
 
-app.get('/javascript', (req, res) => {
-    res.sendFile(__dirname + '/public/javascript.html')
-});
+//global chat room 
 
-app.get('/swift', (req, res) => {
-    res.sendFile(__dirname + '/public/swift.html')
-});
-app.get('/css', (req, res) => {
-    res.sendFile(__dirname + '/public/css.html')
-});
+var roomid = null;
+
+app.get('/room/:uid', function testfn(req, res, next) {
+var name = req.params.uid;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// namespaces
-
-const tech = io.of('/tech');
-
-
-
-tech.on('connection', (socket) => {
-   console.log('user conntected');
-
-   socket.on('join', (data) =>{
-    const sessionID = socket.id;
-
-        socket.join(data.room)
-        // tech.in(data.room).emit('doei' , `${sessionID} joined ${data.room} room!`)
-
-// chat history private message
-
-
-pool.query("SELECT * FROM chats WHERE room = 'javascript' ORDER BY tijd ASC", (err, res) => {
-    if (err) {
-      console.log(err.stack)
-    } else {
-
-    const data = res.rows;
-    data.forEach(row => {
-
-        tech.to(sessionID).emit('history', `${row.username}` , ` ${row.message}`);
-
-    })
-
-
-
-    }
-  })
-
-
-      
-
-
-
-   })
-
-   socket.on('message' , (data) =>{
-    const sessionID = socket.id;
-
-       console.log(`message : ${data.msg}`);
-       tech.in(data.room).emit('message' ,`${sessionID}`, `${data.msg}`);
-
-
-    //insert chat in database
- 
-        console.log(data)
-        // console.log()
-        var sql = `INSERT INTO chats (username, message, room) VALUES ('${sessionID}','${data.msg}','${data.room}')`;
-        pool.query( sql, (err, res) => {
-            console.log('instert')
-        })
-    
- 
+    res.render(__dirname + '/public/chat.html', {name:name})
 
    
+});
 
-   });
 
-   socket.on('disconnect', () =>{
-       console.log('user disconnected');
-    tech.emit('message' , 'user disconnected')
-   })
+const room = io.of('/room');
+
+room.on('connection', (socket) => {
+    socket.on('join', (data) => {
+    
+        console.log(data);
+
+        const sessionID = socket.id;
+
+        socket.join(data.room)
+        room.in(data.room).emit('doei' , `${sessionID} joined ${data.room} room!`)
+
+        // chat history private message
+
+
+        pool.query(`SELECT * FROM chats WHERE room = '${data.room}' ORDER BY tijd ASC`, (err, res) => {
+            if (err) {
+                console.log(err.stack)
+                console.log(res)
+            } else {
+                const data = res.rows;
+                data.forEach(row => {
+
+                    room.to(sessionID).emit('history', `${row.username}`, ` ${row.message}`);
+
+                })
+            }
+        })
+    })
+    socket.on('message', (data) => {
+        const sessionID = socket.id;
+
+        console.log(`message : ${data.msg}`);
+        room.in(data.room).emit('message', `${sessionID}`, `${data.msg}`);
+
+
+        //insert chat in database
+
+        console.log(data)
+        // console.log()
+      
+
+        var sql = `INSERT INTO chats (username, message, room) VALUES ('${sessionID}','${data.msg}','${data.room}')`;
+        // var sql = `INSERT INTO rooms (name) VALUES ('javascript')`;  
+        pool.query(sql, (err, res) => {
+            console.log(sql);
+            console.log(res);
+        })
+    });
+
+    socket.on('disconnect', () => {
+        // console.log('user disconnected');
+        // room.emit('message', 'user disconnected')
+    })
 })
 
+
+//end global chat
+
+//home page section
+const home = io.of('/homepage');
+
+
+home.on('connection', (socket) => {
+    console.log('user conntected');
+
+    socket.on('join', (data) => {
+        const sessionID = socket.id;
+
+        socket.join(data.room)
+        // home.in(data.room).emit('doei' , `${sessionID} joined ${data.room} room!`)
+
+        // chat history private message
+
+
+        pool.query("SELECT * FROM rooms", (err, res) => {
+            if (err) {
+                console.log(err.stack)
+            } else {
+
+                const data = res.rows;
+                data.forEach(row => {
+                    console.log(row);
+                    home.to(sessionID).emit('history', `${row.name}`);
+                    console.log(row.name);
+
+                })
+            }
+        })
+    })
+
+  
+
+    socket.on('disconnect', () => {
+      
+    })
+})
 
 
 
