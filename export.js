e4589727345969d9fd0a9ba891251a29e17b41c1
yes <529521@student.glu.nl>
2020-06-16 15:35:52 +0200

    typo

--- export.js
+++ export.js
@@ -54,7 +54,7 @@
       Client
   } = require('pg')
   const pool = new Pool({
-   user: process.env.SQL_USER ,
+   user: process.env.SQL_USER,
    host: process.env.SQL_HOST,
    database: process.env.SQL_DATABASE,
    password: process.env.SQL_PASSWORD,
--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const {
     Client
 } = require('pg')
 const pool = new Pool({
-    user: process.env.SQL_USER ,
+    user: process.env.SQL_USER,
     host: process.env.SQL_HOST,
     database: process.env.SQL_DATABASE,
     password: process.env.SQL_PASSWORD,
